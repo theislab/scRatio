60ff83814e5054c314ad12a1bacbb4c6f5cb603c
allepalma <alessandro.palma1998@hotmail.it>
2026-01-21 10:36:39 +0100

    Differential abundance experiments

---
+++ experiments/differential_abundance/scRatio/train_scRatio.py
@@ -0,0 +1,147 @@
+import sys
+import traceback
+from pathlib import Path
+import numpy as np
+import scanpy as sc
+import torch
+from torch.utils.data import DataLoader, TensorDataset
+import lightning as L
+from torchdyn.core import NeuralODE
+from sklearn.preprocessing import OneHotEncoder
+import hydra
+from omegaconf import DictConfig
+from tqdm.auto import tqdm
+from utils import *
+
+torch.set_float32_matmul_precision("medium")
+
+def train(batch_size, n_steps, model, optimizer, X, C):
+    for k in tqdm(range(n_steps)):
+        optimizer.zero_grad()
+    
+        indices = np.random.choice(range(X.shape[0]), size=batch_size, replace=False)
+        loss = model.shared_step(X[indices], C[indices], k)
+        
+        loss.backward()
+        optimizer.step()
+    return model
+
+def compute_ratio(model, data_samples, cond, cond_dim, condition, control, batch_size):
+    # Initialize the device 
+    device = data_samples.device   
+
+    # Initialize torch dataloader to iterate through the samples 
+    dataloader = DataLoader(TensorDataset(data_samples, cond), batch_size=batch_size, drop_last=False)
+    log_ratios = []
+    for batch in tqdm(dataloader):
+        X_batch = batch[0]
+        C_batch = batch[1]
+        
+        # correction term - its own field
+        node = NeuralODE(
+            NODEWrapper_with_ratio_tvf_rl(model, control=torch.tensor(control).float().expand(X_batch.shape[0], cond_dim).to(device),
+                condition=torch.tensor(condition).float().expand(X_batch.shape[0], cond_dim).to(device), point=C_batch),
+            solver="dopri5", sensitivity="adjoint", atol=1e-4, rtol=1e-4
+        )
+        
+        with torch.no_grad():
+            traj = node.trajectory(
+                torch.cat([X_batch, torch.zeros(X_batch.shape[0], 1).to(device)], dim=-1),
+                t_span=torch.linspace(1, 0, 2).to(device)
+            )
+        _, ratio = traj[-1][:, :-1], traj[-1][:, -1]
+        log_ratio_hat = -ratio.cpu().numpy()
+        log_ratios.append(log_ratio_hat)
+
+    return np.concatenate(log_ratios)
+
+def train_scratio(adata: sc.AnnData, scheduler_type: str, res_dir: Path, run_name: str):
+    # Fix number of PCs
+    N_pcs = 10 
+    
+    # Observatoions and conditioning variables
+    X = torch.from_numpy(adata.obsm["X_pca"][:, :N_pcs]).float().cuda()
+    C = OneHotEncoder().fit_transform(adata.obs[["treatment"]]).toarray()
+    C = torch.from_numpy(C).float().cuda()
+    
+    if scheduler_type == "deterministic":
+        sigma = 0
+        sigma_min = 0
+    elif scheduler_type == "deterministic_sigma_min":
+        sigma = 0
+        sigma_min = 0.1
+    elif scheduler_type == "stochastic":
+        sigma = 0.01
+        sigma_min = 0
+    else:
+        raise NotImplementedError
+    
+    # Initialize schedules 
+    lambda_t = lambda t: torch.sqrt((1 - (1 - sigma_min) * t) ** 2 + sigma * t * (1 - t))
+    lambda_sp_t = lambda t: (sigma * (1 - 2 * t) - 2 * (1 - sigma_min) * (1 - (1 - sigma_min) * t)) / 2
+    
+    # Training setup 
+    n_steps = 150_000
+    batch_size = 1024
+
+    # Model initialization 
+    model = ConditionalFlowMatchingWithScore(
+        input_dim=N_pcs,
+        cond_dims=[2],
+        hidden_dims=[1024, 1024, 1024],
+        encoder_hidden_dims=[256],
+        encoder_out_dim=256,
+        encoder_out_dim_cond=50,
+        use_sinusoidal_embeddings=True,
+        sinusoidal_feature_dim=50,
+        lambda_t=lambda_t,
+        lambda_sp_t=lambda_sp_t,
+        betas=[0], 
+        lr=1e-4
+    ).to("cuda")
+    optimizer = model.configure_optimizers()
+    model = train(batch_size, n_steps, model, optimizer, X, C)
+    
+    # Compute ratio
+    control = np.array([1, 0])
+    condition = np.array([0, 1])
+    log_ratio = compute_ratio(model, X, C, 2, condition, control, batch_size=1000)
+    adata.obs["log_ratios"] = log_ratio 
+    
+    # Save results 
+    run_dir = res_dir / scheduler_type
+    run_dir.mkdir(parents=True, exist_ok=True)
+    adata.write_h5ad(run_dir / f"{run_name}.h5ad") 
+
+@hydra.main(config_path="/home/icb/alessandro.palma/environment/scFM_density_estimation/experiments/differential_abundance/scRatio/config", config_name="train", version_base=None)
+def main(config: DictConfig):
+    # Initialize directiories and adata path 
+    res_dir = Path(config.paths.res_dir)  
+    res_dir.mkdir(parents=True, exist_ok=True)  # make directory if doesn't exist
+    
+    # Read AnnData
+    adata_path = Path(config.paths.adata_path)
+    
+    # Scheduler parameters 
+    scheduler_type = config.scheduler.scheduler_type  # Deterministic or stochastic 
+
+    # Read AnnData 
+    base_adata = sc.read_h5ad(adata_path)
+    adata_fname = adata_path.name
+    tag = adata_fname.replace(".h5ad", "").split("_")[1]
+
+    for i in range(3):
+        run_name = f"oversamp_{tag}_{i}"
+
+        # IMPORTANT: copy AnnData for each run
+        adata = base_adata.copy()
+
+        train_scratio(adata, scheduler_type, res_dir, run_name)
+    
+if __name__ == "__main__":
+    # running the experiment
+    try: 
+        main()
+    except Exception as e:
+        traceback.print_exc()
+        sys.exit(1)
---
+++ notebooks/differential_abundance_analysis/model_evaluation.py
@@ -0,0 +1,143 @@
+import numpy as np
+import pandas as pd
+
+import scanpy as sc
+
+import scipy as sp
+from sklearn.metrics import precision_recall_curve, auc
+
+def compute_precision_and_recall_auc(predicted, true):
+    # Compute precision and recall curve and AUC
+    precision, recall, _ = precision_recall_curve(true, predicted)
+    pr_auc = auc(recall, precision)
+    return pr_auc
+
+def cluster_metrics(abs_ratios, ratios, is_abundant, leiden_labels):
+    # Non-abundant clusters should always be lower 
+    non_abundant_over_abundant = abs_ratios[~is_abundant].mean() / abs_ratios[is_abundant].mean()
+    # Compute fraction correct sign
+    ratios_abundant = ratios[is_abundant]  # Only clusters 1 and 2 
+    leiden_abundant = np.array(leiden_labels)[is_abundant]
+
+    leiden_abundant_unique = np.unique(leiden_abundant)
+    assert "0" not in leiden_abundant_unique
+    assert "3" not in leiden_abundant_unique
+    
+    ratios_abundant[leiden_abundant=="2"] = ratios_abundant[leiden_abundant=="2"] * (-1)
+    correct_sign_prop = (ratios_abundant >= 0).sum() / len(ratios_abundant)
+    
+    # Kolmogorov-Smirnov test 
+    ks_D, _ = sp.stats.ks_2samp(
+        abs_ratios[is_abundant],
+        abs_ratios[~is_abundant],
+        alternative="two-sided",
+        mode="asymp"
+    )
+    w_distance = sp.stats.wasserstein_distance(abs_ratios[is_abundant], abs_ratios[~is_abundant])
+    return non_abundant_over_abundant, correct_sign_prop, ks_D, w_distance
+    
+def compute_evaluation_metrics(result_path, model, subsampling_rates, n_runs):
+    results_per_oversamp = {
+        "p_major_cluster": [], 
+        "p_minor_cluster": [],
+        "auc_score": [], 
+        "non_abundant_over_abundant": [],
+        "correct_sign_prop": [],
+        "ks_D": [], 
+        "w_distance": [],
+        "run": []
+        }
+    
+    results_per_run = {
+        "metric": [], 
+        "value": [],
+        "run": []
+        }
+
+    for i in range(n_runs):
+        # Compute the ratios and abundances per cluster
+        per_cluster_ratios = []
+        per_cluster_log_odds = []
+        
+        for r_oversamp in subsampling_rates:
+            # Compute the true per-cluster ratio 
+            p = 0.5 - r_oversamp  # Always minor cluster prop
+            one_minus_p = 1 - p  # Always major cluster prop
+    
+            # Collect results and compute AUC 
+            if model=="mrvi":
+                adata_generated = sc.read_h5ad(result_path / f"oversamp_{r_oversamp}_{i}" / f"oversamp_{r_oversamp}_{i}.h5ad")
+            elif model=="meld":
+                adata_generated = sc.read_h5ad(result_path / f"oversamp_{r_oversamp}.h5ad")
+            else:
+                adata_generated = sc.read_h5ad(result_path / f"oversamp_{r_oversamp}_{i}.h5ad")
+            
+            # Collect abundance labels 
+            is_abundant = np.logical_or(adata_generated.obs.leiden=="1", adata_generated.obs.leiden=="2")
+            if model=="mrvi":
+                abs_ratio = np.abs(adata_generated.obs[f"log_ratio_oversamp_{r_oversamp}_{i}"]).values
+                ratio = adata_generated.obs[f"log_ratio_oversamp_{r_oversamp}_{i}"].values
+            elif model=="scRatio":
+                abs_ratio = np.abs(adata_generated.obs["log_ratios"]).values
+                ratio = adata_generated.obs["log_ratios"].values
+            else:
+                abs_ratio = np.abs(adata_generated.obs["log_ratio"]).values
+                ratio = adata_generated.obs["log_ratio"].values
+
+            if model=="milo":
+                adata_generated = adata_generated[~np.isnan(abs_ratio)]
+                is_abundant = is_abundant[~np.isnan(abs_ratio)]
+                abs_ratio = abs_ratio[~np.isnan(abs_ratio)]
+                ratio = ratio[~np.isnan(ratio)]
+
+            # Compute AUC
+            auc = compute_precision_and_recall_auc(abs_ratio, is_abundant)
+            results_per_oversamp["p_major_cluster"].append(one_minus_p)
+            results_per_oversamp["p_minor_cluster"].append(p)
+            results_per_oversamp["auc_score"].append(auc)
+            results_per_oversamp["run"].append(i)
+            
+            for cl in ["0", "1", "2", "3"]:
+                per_cluster_ratios.append(np.mean(ratio[adata_generated.obs["leiden"]==cl]))
+
+                numerator = (adata_generated.obs.loc[adata_generated.obs["leiden"]==cl, "treatment"]==1).sum()
+                denominator = (adata_generated.obs.loc[adata_generated.obs["leiden"]==cl, "treatment"]==0).sum() 
+
+                abs_log_odds = np.log((numerator + 1e-9) / (denominator + 1e-9))
+                per_cluster_log_odds.append(abs_log_odds)
+            
+            # Compute cluster metrics
+            abundant_over_non_abundant, correct_sign_prop, ks_D, w_distance = cluster_metrics(abs_ratio, ratio, is_abundant, adata_generated.obs["leiden"])
+            results_per_oversamp["non_abundant_over_abundant"].append(abundant_over_non_abundant)
+            results_per_oversamp["correct_sign_prop"].append(correct_sign_prop)
+            results_per_oversamp["ks_D"].append(ks_D)
+            results_per_oversamp["w_distance"].append(w_distance)
+        
+        results_per_run["metric"].append("spearman_log_odds")
+        results_per_run["run"].append(i)
+        results_per_run["value"].append(sp.stats.spearmanr(per_cluster_ratios, per_cluster_log_odds)[0])
+        results_per_run["metric"].append("pearson_log_odds")
+        results_per_run["run"].append(i)
+        results_per_run["value"].append(sp.stats.pearsonr(per_cluster_ratios, per_cluster_log_odds)[0])
+
+    # After computing the metrics per run and abundance, compute metrics per run only. 
+    results_per_oversamp_df = pd.DataFrame(results_per_oversamp)
+    for i in range(n_runs):
+        # Take df at run i 
+        results_per_oversamp_df_i = results_per_oversamp_df.loc[results_per_oversamp_df.run==i]
+        for metric in results_per_oversamp_df_i.columns:
+            name = f"corr_{metric}_p"
+            results_per_run["metric"].append(name)
+            results_per_run["run"].append(i) 
+            results_per_run["value"].append(sp.stats.pearsonr(results_per_oversamp_df_i[metric],
+                                                 results_per_oversamp_df_i["p_major_cluster"])[0])
+
+            name = f"mean_{metric}"
+            results_per_oversamp_df_over_i_03 = results_per_oversamp_df_i[results_per_oversamp_df_i.p_major_cluster>0.8]
+            results_per_run["metric"].append(name)
+            results_per_run["run"].append(i) 
+            results_per_run["value"].append(results_per_oversamp_df_over_i_03[metric].mean())
+        
+    # Results per data frame 
+    results_per_run_df = pd.DataFrame(results_per_run)    
+    return results_per_oversamp_df, results_per_run_df
