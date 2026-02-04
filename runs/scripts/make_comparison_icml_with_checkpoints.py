9d1ad111c4b71dee8105df175ee8547e37ca0686
Egor Antipov <egor.antipov@hpc-build01.scidom.de>
2026-02-04 13:44:07 +0100

    icml tests

--- runs/scripts/make_comparison_icml_with_checkpoints.py
+++ runs/scripts/make_comparison_icml_with_checkpoints.py
@@ -29,6 +29,25 @@ def prepare_dataset(n, N, cond_dim, locs):
     C_test = F.one_hot(torch.tensor(C_test).long(), num_classes=cond_dim).to("cuda").float()
     return X_train, X_test, C_train, C_test
 
+def sinusoidal_time_features(t: torch.Tensor, num_freqs: int = 128, max_period: int = 10000):
+    if len(t.shape)==1:
+        t = t.unsqueeze(1)
+        
+    half = num_freqs // 2
+    freqs = torch.exp(
+        -math.log(max_period)
+        * torch.arange(start=0, 
+                       end=half, 
+                       dtype=torch.float32, 
+                       device=t.device)
+        / half
+    )
+    args = t.float() * freqs[None]
+    embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
+    if num_freqs % 2:
+        embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
+    return embedding
+
 class Encoder(nn.Module):
     def __init__(self, cond_dim: int = 1, cond_hidden_dims: list = [],
                  cond_out_dim: int = 2, dropout: float = 0):
@@ -57,10 +76,12 @@ class ConditionalFlowMatchingWithScore(L.LightningModule):
         hidden_dims: list,
         encoder_hidden_dims: list,
         encoder_out_dim: int,
+        encoder_out_dim_cond: int, 
+        time_feature_dim: int, 
         lambda_t: Callable,
         lambda_sp_t: Callable,
         betas: list,
-        lr: float = 1e-3,
+        lr: float = 1e-4,
         use_ot_sampler: bool = False,
         ot_method: str = "exact",
         dropout: float = 0
@@ -70,17 +91,18 @@ class ConditionalFlowMatchingWithScore(L.LightningModule):
 
         self.data_encoder = Encoder(input_dim, encoder_hidden_dims, encoder_out_dim, dropout)
         self.cond_encoders = nn.ModuleList([
-            Encoder(cond_dim, encoder_hidden_dims, encoder_out_dim, dropout)
+            Encoder(cond_dim, encoder_hidden_dims, encoder_out_dim_cond, dropout)
             for cond_dim in cond_dims
         ])
-        self.vf_mlp = FlowMatchingMLP(encoder_out_dim + 1, hidden_dims, input_dim, dropout)
-        self.score_mlp = FlowMatchingMLP(encoder_out_dim + 1, hidden_dims, input_dim, dropout)
+        self.vf_mlp = FlowMatchingMLP(encoder_out_dim + encoder_out_dim_cond * len(cond_dims) + time_feature_dim, hidden_dims, input_dim, dropout)
+        self.score_mlp = FlowMatchingMLP(encoder_out_dim + encoder_out_dim_cond * len(cond_dims) + time_feature_dim, hidden_dims, input_dim, dropout)
         
         self.lambda_t = lambda_t
         self.lambda_sp_t = lambda_sp_t
-
+        self.time_feature_dim = time_feature_dim
         self.betas = betas
         self.encoder_out_dim = encoder_out_dim
+        self.encoder_out_dim_cond = encoder_out_dim_cond
         self.use_ot_sampler = use_ot_sampler
         self.cond_dims = cond_dims
         self.lr = lr
@@ -90,19 +112,25 @@ class ConditionalFlowMatchingWithScore(L.LightningModule):
             t = t.expand(x.shape[0]).unsqueeze(1)
         elif t.dim() == 1:
             t = t.unsqueeze(1)
-        
+
         start = 0
         xc = self.data_encoder(x)
         for i, cond_dim in enumerate(self.cond_dims):
             if use_conds[i]:
-                xc += self.cond_encoders[i](cond[:, start:(start + cond_dim)])
+                cond_enc = self.cond_encoders[i](cond[:, start:(start + cond_dim)])
+            else:
+                cond_enc = torch.zeros(xc.shape[0], self.encoder_out_dim_cond).to(xc.device).float()
+                
+            xc = torch.cat([xc, cond_enc], dim=1)
             start += cond_dim
-        
-        xtc = torch.cat([xc, t], dim=1)
+
+        if self.time_feature_dim > 1:
+            xtc = torch.cat([xc, sinusoidal_time_features(t, self.time_feature_dim)], dim=1)
+        else:
+            xtc = torch.cat([xc, t], dim=1)
         
         vf = self.vf_mlp.mlp(xtc)
         score = self.score_mlp.mlp(xtc)
-        
         return vf, score
 
     def shared_step(self, x1, cond):
@@ -257,19 +285,53 @@ def evaluate_model(model, data_samples, cond, cond_dim, condition, control, locs
     
     return [log_ratio_true, log_ratio_hat, log_ratio_hat_v2], [time_true, time_hat, time_hat_v2]
 
+def get_params(num_dims):
+    if num_dims == 2:
+        time_feature_dim = 32
+        latent_dim = 128
+        cond_latent_dim = 64
+    elif num_dims == 5:
+        time_feature_dim = 1
+        latent_dim = 256
+        cond_latent_dim = 64
+    elif num_dims == 10:
+        time_feature_dim = 1
+        latent_dim = 256
+        cond_latent_dim = 128
+    elif num_dims == 20:
+        time_feature_dim = 32
+        latent_dim = 64
+        cond_latent_dim = 32
+    elif num_dims == 30:
+        time_feature_dim = 1
+        latent_dim = 128
+        cond_latent_dim = 64
+    elif num_dims == 50:
+        time_feature_dim = 32
+        latent_dim = 256
+        cond_latent_dim = 32
+    else:
+        raise ValueError("Unknown num_dims value")
+        
+    return latent_dim, time_feature_dim, cond_latent_dim
+
 @hydra.main(config_path="../configs", config_name="base", version_base=None)
 def main(cfg: DictConfig):
+    np.random.seed(42)
+
     n = cfg.num_dims
-    N = 30_000
+    N = 100_000
     cond_dim = 2
     batch_size = 512
-    n_steps = 50_000
-    n_tries = 10
+    n_steps = 100_000
+    n_tries = 5
     control = np.array([1, 0])
     condition = np.array([0, 1])
     locs = [[0 for _ in range(n)]] + [[cfg.loc for _ in range(n)]]
-    sigmas = [1, 0.75, 0.5, 0.25, 0]
+    sigma = 0
     sigma_mins = [1e-1, 1e-2, 1e-3, 1e-4, 0]
+
+    latent_dim, time_feature_dim, cond_latent_dim = get_params(cfg.num_dims)
 
     names = ["true", "naive", "ct own rl"]
 
@@ -278,7 +340,6 @@ def main(cfg: DictConfig):
 
         results = {}
 
-        sigma_last = 2
         sigma_min_last = 2
     else:
         with open(f"./runs/scripts/checkpoints/checkpoint_{cfg.loc}_{cfg.num_dims}.pkl", "rb") as f:
@@ -292,74 +353,132 @@ def main(cfg: DictConfig):
         C_train = saved_vars["C_train"]
         C_test = saved_vars["C_test"]
 
-    for sigma in sigmas:
-        for sigma_min in sigma_mins:
-            if (sigma >= sigma_last) and (sigma_min >= sigma_min_last):
-                print(f"Skipping sigma={sigma}, sigma_min={sigma_min} since already done")
-                continue
-            else:
-                print(f"Starting sigma={sigma}, sigma_min={sigma_min}")
+    for sigma_min in sigma_mins:
+        if sigma_min >= sigma_min_last:
+            print(f"Skipping sigma={sigma}, sigma_min={sigma_min} since already done")
+            continue
+        else:
+            print(f"Starting sigma={sigma}, sigma_min={sigma_min}")
 
-            lambda_t = lambda t: torch.sqrt((1 - (1 - sigma_min) * t) ** 2 + sigma * t * (1 - t))
-            lambda_sp_t = lambda t: (sigma * (1 - 2 * t) - 2 * (1 - sigma_min) * (1 - (1 - sigma_min) * t)) / 2
-            
-            tmp_results = {name: np.array([]) for name in names}
-            time_results = {name: [] for name in names}
-            for _ in tqdm(range(n_tries)):
-                model = ConditionalFlowMatchingWithScore(
-                    input_dim=n,
-                    cond_dims=[cond_dim],
-                    hidden_dims=[1024, 1024, 1024],
-                    encoder_hidden_dims=[256],
-                    encoder_out_dim=50,
-                    lambda_t=lambda_t,
-                    lambda_sp_t=lambda_sp_t,
-                    betas=[0.1]
-                ).to("cuda")
-                optimizer = model.configure_optimizers()
-                
-                model = train(batch_size, n_steps, model, optimizer, X_train, C_train)
-                log_ratios, times = evaluate_model(model, X_test, C_test, cond_dim, condition, control, locs)
+        lambda_t = lambda t: torch.sqrt((1 - (1 - sigma_min) * t) ** 2 + sigma * t * (1 - t))
+        lambda_sp_t = lambda t: (sigma * (1 - 2 * t) - 2 * (1 - sigma_min) * (1 - (1 - sigma_min) * t)) / 2
         
-                for i, name in enumerate(names):
-                    if tmp_results[name].shape[0] == 0:
-                        tmp_results[name] = log_ratios[i].reshape(-1, 1)
-                    else:
-                        tmp_results[name] = np.concatenate([tmp_results[name], log_ratios[i].reshape(-1, 1)], axis=1)
-                        
-                    time_results[name].append(times[i])
-                        
-            for name in names:
-                if name == "true":
-                    key =  "- - true"
-                elif name == "naive":
-                    key = str(sigma_min) + " " + str(sigma) + " naive"
-                elif name == "ct own rl":
-                    key = str(sigma_min) + " " + str(sigma) + " rl"
+        tmp_results = {name: np.array([]) for name in names}
+        time_results = {name: [] for name in names}
+        for _ in tqdm(range(n_tries)):
+            model = ConditionalFlowMatchingWithScore(
+                input_dim=n,
+                cond_dims=[cond_dim],
+                hidden_dims=[1024, 1024, 1024],
+                encoder_hidden_dims=[256],
+                encoder_out_dim=latent_dim,
+                lambda_t=lambda_t,
+                lambda_sp_t=lambda_sp_t,
+                betas=[0],
+                lr=1e-4,
+                time_feature_dim=time_feature_dim,
+                encoder_out_dim_cond=cond_latent_dim,
+            ).to("cuda")
+            optimizer = model.configure_optimizers()
+            
+            model = train(batch_size, n_steps, model, optimizer, X_train, C_train)
+            log_ratios, times = evaluate_model(model, X_test, C_test, cond_dim, condition, control, locs)
+    
+            for i, name in enumerate(names):
+                if tmp_results[name].shape[0] == 0:
+                    tmp_results[name] = log_ratios[i].reshape(-1, 1)
                 else:
-                    raise ValueError("Unknown value for name")
-                
-                if key in results:
-                    results[key]["value"] = np.concatenate([results[key]["value"], tmp_results[name]], axis=1)
-                    results[key]["time"] += time_results[name]
+                    tmp_results[name] = np.concatenate([tmp_results[name], log_ratios[i].reshape(-1, 1)], axis=1)
+                    
+                time_results[name].append(times[i])
+                    
+        for name in names:
+            if name == "true":
+                key =  "- - true"
+            elif name == "naive":
+                key = str(sigma_min) + " " + str(sigma) + " naive"
+            elif name == "ct own rl":
+                key = str(sigma_min) + " " + str(sigma) + " rl"
+            else:
+                raise ValueError("Unknown value for name")
+            
+            if key in results:
+                results[key]["value"] = np.concatenate([results[key]["value"], tmp_results[name]], axis=1)
+                results[key]["time"] += time_results[name]
+            else:
+                results[key] = {}
+                results[key]["value"] = tmp_results[name]
+                results[key]["time"] = time_results[name]
+
+        sigma_last = sigma
+        sigma_min_last = sigma_min
+
+        with open(f"./runs/scripts/checkpoints/checkpoint_{cfg.loc}_{cfg.num_dims}.pkl", "wb") as f:
+            pickle.dump({
+                "results": results,
+                "sigma_last": sigma_last,
+                "sigma_min_last": sigma_min_last,
+                "X_train": X_train,
+                "X_test": X_test,
+                "C_train": C_train,
+                "C_test": C_test
+            }, f)
+
+    sigmas = [1, 0.75, 0.5, 0.25]
+    sigma_min = 0
+
+    for sigma in sigmas:
+        print(f"Starting sigma={sigma}, sigma_min={sigma_min}")
+        
+        lambda_t = lambda t: torch.sqrt((1 - (1 - sigma_min) * t) ** 2 + sigma * t * (1 - t))
+        lambda_sp_t = lambda t: (sigma * (1 - 2 * t) - 2 * (1 - sigma_min) * (1 - (1 - sigma_min) * t)) / 2
+        
+        tmp_results = {name: np.array([]) for name in names}
+        time_results = {name: [] for name in names}
+        for _ in tqdm(range(n_tries)):
+            model = ConditionalFlowMatchingWithScore(
+                input_dim=n,
+                cond_dims=[cond_dim],
+                hidden_dims=[1024, 1024, 1024],
+                encoder_hidden_dims=[256],
+                encoder_out_dim=latent_dim,
+                lambda_t=lambda_t,
+                lambda_sp_t=lambda_sp_t,
+                betas=[0],
+                lr=1e-4,
+                time_feature_dim=time_feature_dim,
+                encoder_out_dim_cond=cond_latent_dim,
+            ).to("cuda")
+            optimizer = model.configure_optimizers()
+            
+            model = train(batch_size, n_steps, model, optimizer, X_train, C_train)
+            log_ratios, times = evaluate_model(model, X_test, C_test, cond_dim, condition, control, locs)
+
+            for i, name in enumerate(names):
+                if tmp_results[name].shape[0] == 0:
+                    tmp_results[name] = log_ratios[i].reshape(-1, 1)
                 else:
-                    results[key] = {}
-                    results[key]["value"] = tmp_results[name]
-                    results[key]["time"] = time_results[name]
-
-            sigma_last = sigma
-            sigma_min_last = sigma_min
-
-            with open(f"./runs/scripts/checkpoints/checkpoint_{cfg.loc}_{cfg.num_dims}.pkl", "wb") as f:
-                pickle.dump({
-                    "results": results,
-                    "sigma_last": sigma_last,
-                    "sigma_min_last": sigma_min_last,
-                    "X_train": X_train,
-                    "X_test": X_test,
-                    "C_train": C_train,
-                    "C_test": C_test
-                }, f)
+                    tmp_results[name] = np.concatenate([tmp_results[name], log_ratios[i].reshape(-1, 1)], axis=1)
+                    
+                time_results[name].append(times[i])
+                    
+        for name in names:
+            if name == "true":
+                key =  "- - true"
+            elif name == "naive":
+                key = str(sigma_min) + " " + str(sigma) + " naive"
+            elif name == "ct own rl":
+                key = str(sigma_min) + " " + str(sigma) + " rl"
+            else:
+                raise ValueError("Unknown value for name")
+            
+            if key in results:
+                results[key]["value"] = np.concatenate([results[key]["value"], tmp_results[name]], axis=1)
+                results[key]["time"] += time_results[name]
+            else:
+                results[key] = {}
+                results[key]["value"] = tmp_results[name]
+                results[key]["time"] = time_results[name]
 
     mask_condition = np.all(C_test.cpu().numpy() == condition, axis=1)
     mask_control = np.all(C_test.cpu().numpy() == control, axis=1)
@@ -369,7 +488,7 @@ def main(cfg: DictConfig):
     results["mask_control"] = mask_control
     results["mask_both"] = mask_both
 
-    with open(f"/home/icb/egor.antipov/scFM_density_estimation/notebooks/icml_tests/table_results/results_icml_{cfg.loc}_{cfg.num_dims}.pkl", "wb") as f:
+    with open(f"/home/icb/egor.antipov/scFM_density_estimation/notebooks/icml_tests/table_results/results_icml_{cfg.loc}_{cfg.num_dims}_swept2.pkl", "wb") as f:
         pickle.dump(results, f)
 
     print("FINISHED")
